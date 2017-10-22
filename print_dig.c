bacf9b3845b48379ecc9a856b3548eee1fa5c5e5
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-22 22:20:26 +0000

    Added return test and reinput buffer variable

--- print_dig.c
+++ print_dig.c
@@ -11,26 +11,28 @@ int print_dig(va_list args)
 	int n;
 	int divisor;
 	int count;
-	unsigned int val;
 	unsigned int num;
+	unsigned int val;
+	int returnsize;
 
 	divisor = 1; /** Begins breakdown of large number with division by 1 */
 	n = va_arg(args, int);
-	num = n; /** Gives temporary buffer for value passed */
+	num = n; /** Allows greater val to be held */
 
 	if (n < 0) /** If value passed is negative */
 	{
 		num = -n;
-		val = num;
+		val = num; /** Holds value of num while num is analyzed */
 		_putchar('-');
 	}
-	else /** If value passed is positive */
+	else
 		val = num;
-	for (count = 0; num / 10 > 0; count++) /** Loops through number of numbers */
+	for (count = 0; num / 10 > 0; count++) /** Decides how big number is */
 	{
 		num = num / 10;
 		divisor = divisor * 10; /** Increments divisor with each passing */
 	}
+	returnsize = count; /** Will return count of characters to main fxn */
 	while (count > 0)
 	{
 		_putchar(val / divisor + '0');
@@ -39,5 +41,5 @@ int print_dig(va_list args)
 		--count;
 	}
 	_putchar(val + '0'); /** To convert from ASCII */
-	return (1);
+	return (returnsize);
 }
