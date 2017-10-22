ef9104f2e9ac0d1b716c5598cf014943a1f20270
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-22 21:57:01 +0000

    Added tester function

--- main.c
+++ main.c
@@ -6,8 +6,11 @@ int main(void)
 	char c = '9';
 	char *s = "Linds";
 	char *name = "Lee";
+	int n;
 	int x;
 
+	n = 5;
+	_printf("%d\n", n);
 	x = _printf("Hi %s, my name is %s.\nChar: %c\nPercent: %%\n", s, name, c);
 	printf("count (echo $?) = %d\n", x);
 	return (x);
