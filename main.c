a8f60b310c37ab68b7e67adf11dfd8fef7af6276
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-22 21:39:49 +0000

    Added two test functions

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ int main(void)
 	char *name = "Lee";
 	int x;
 
+	_printf("Negative:[%d]\n", -709209);
+	_printf("Int size: %d\n", 645235);
 	x = _printf("Hi %s, my name is %s.\nChar: %c\nPercent: %%\n", s, name, c);
 	printf("count (echo $?) = %d\n", x);
 	return (x);
