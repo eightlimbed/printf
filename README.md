4114515bae1f1e1bc4db37a113087625a879b33e
Lee Gaines <eightlimbed@gmail.com>
2017-10-21 20:49:41 -0700

    initial commit - adds README, _putchar, and header file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Low Level Programming
+#### Assignment 0x10. C - printf
+---
+File Name | Description | Language(s)
+--- | --- | ---
+
---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: character
+ *
+ * Return: 1 (success), -1 (error)
+ */
+
+int _putchar(char c)
+{
+	/* writes 1 byte from the address of c to stdout (file descriptor 1) */
+	return (write(1, &c, 1));
+}
