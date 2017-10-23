d1c3bcfedc4fcb7f0ed63aefa69b994334fbde7a
Lee Gaines <eightlimbed@gmail.com>
2017-10-23 11:55:20 -0700

    updates _printf, removes print_dig, int, and percent

--- _printf.c
+++ _printf.c
@@ -13,21 +13,16 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	unsigned int i, j, count, special_mode;
+	unsigned int i, j, count;
+	spec arr[] = {{"c", print_char}, {"s", print_string}, {NULL, NULL} };
 
-	spec arr[] = {
-		{"c", print_char}, {"s", print_string}, {"%", print_percent},
-		{"d", print_dig}, {"i", print_int}, {NULL, NULL}
-	};
 	if (format == NULL)
-		return (0);
+		return (-1);
 	va_start(args, format);
-	special_mode = 0, count = 0, i = 0;
+	count = i = 0;
 	while (format[i])
 	{
 		if (format[i] == '%')
-			special_mode = 1;
-		if (special_mode)
 		{
 			j = 0;
 			while (arr[j].special != NULL)
@@ -39,14 +34,20 @@ int _printf(const char *format, ...)
 				}
 				j++;
 			}
+			if (arr[j].special == NULL)
+			{
+				if (format[i] == '%' && format[i + 1] != '%')
+				{
+					_putchar('%');
+					count++;
+				}
+			}
 		}
-		if (!(special_mode))
+		else
 		{
 			_putchar(format[i]);
 			count++;
 		}
-		else
-			special_mode = 0;
 		i++;
 	}
 	va_end(args);
