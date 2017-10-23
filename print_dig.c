f5708a19a2be2635f8975298f75dc2d4454886b1
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-10-23 22:50:05 +0000

    Accounted for return size if passed negative

--- print_dig.c
+++ print_dig.c
@@ -10,10 +10,10 @@ int print_dig(va_list args)
 {
 	int n;
 	int divisor;
-	int count;
+	int count = 0;
 	unsigned int num;
 	unsigned int val;
-	int retsize;
+	int retsize = 0;
 
 	divisor = 1; /** Begins breakdown of large number with division by 1 */
 	n = va_arg(args, int);
@@ -21,18 +21,18 @@ int print_dig(va_list args)
 
 	if (n < 0) /** If value passed is negative */
 	{
-		num = -n;
+		num = n * -1;
 		val = num; /** Holds value of num while num is analyzed */
 		_putchar('-');
 	}
 	else
 		val = num;
-	for (count = 0; num / 10 > 0; count++) /** Decides how big number is */
+	for (; num / 10 > 0; count++) /** Decides how big number is */
 	{
 		num = num / 10;
 		divisor = divisor * 10; /** Increments divisor with each passing */
 	}
-	retsize = count; /** Will return count of characters to main fxn */
+	retsize = count + 1; /** Will return count of characters to main fxn */
 	while (count > 0)
 	{
 		_putchar(val / divisor + '0');
@@ -40,6 +40,8 @@ int print_dig(va_list args)
 		divisor = divisor / 10;
 		--count;
 	}
-	_putchar(val + '0'); /** To convert from ASCII */
+	if (n < 0)
+		retsize++;
+	_putchar(val + '0');
 	return (retsize);
 }
